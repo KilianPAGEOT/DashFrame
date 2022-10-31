5892bb96addff0e7a0aa73fbb8330ce2a9882355
kilianpageot <kilian.pageot@epitech.eu>
2022-10-31 10:25:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+DASHFRAME  
+===
+
+wip
---
+++ src/main/java/com/github/dashframe/DashframeApplication.java
@@ -0,0 +1,13 @@
+package com.github.dashframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashframeApplication.class, args);
+	}
+
+}
