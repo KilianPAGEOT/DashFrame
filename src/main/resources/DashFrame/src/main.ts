018fc28b466d5c50e736ade8a0411790d3d57bc3
Mathiso00 <mathis.onillon@epitech.eu>
2022-11-03 11:37:00 +0100

    Setup fonctionnal Quasar

--- src/main/resources/DashFrame/src/main.ts
+++ src/main/resources/DashFrame/src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
+import "@quasar/extras/material-icons/material-icons.css";
 
+// Import Quasar css
+import "quasar/src/css/index.sass";
 //import "./assets/main.css";
 
 createApp(App)
