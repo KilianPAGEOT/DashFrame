ebf40b2be96306cc90d2e70fc38fdb88eaafe6c4
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-03 12:33:52 +0100

    Add stub API endpoints for `/about.json` and `/services`

--- src/main/java/com/github/dashframe/DashframeApplication.java
+++ src/main/java/com/github/dashframe/DashframeApplication.java
@@ -1,13 +1,31 @@
 package com.github.dashframe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@Controller
+@ResponseBody
+@EnableAutoConfiguration
 public class DashframeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DashframeApplication.class, args);
 	}
 
+	@GetMapping("/about.json")
+	public ResponseEntity<Map<String, Object>> aboutJson() {
+		Map<String, Object> body = new HashMap<>();
+		body.put("client", new HashMap<>());
+		body.put("server", new HashMap<>());
+		return ResponseEntity.ok(body);
+	}
 }
