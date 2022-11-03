912bfc1380ed2c0d21c68ad65ffc9730c0f4139e
Mathiso00 <mathis.onillon@epitech.eu>
2022-11-03 10:15:11 +0100

    Setup Vue app with header

--- src/main/resources/DashFrame/src/App.vue
+++ src/main/resources/DashFrame/src/App.vue
@@ -1,53 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import MainHeaderVue from "./components/MainHeader.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <MainHeaderVue msg="DashFrame" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <q-btn color="amber" glossy label="Amber" />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+html > body {
+  margin: 0px;
 }
 </style>
