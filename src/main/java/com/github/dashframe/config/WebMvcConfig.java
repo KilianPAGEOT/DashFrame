ec3d7d49d986a37b8c1c57e67fda6759921bab7f
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-04 09:25:24 +0100

    Wrap the front-end as a Gradle subproject

--- src/main/java/com/github/dashframe/config/WebMvcConfig.java
+++ src/main/java/com/github/dashframe/config/WebMvcConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-  private static final String API_VERSION = "v1";
-  private static final String API_BASE = "api/" + API_VERSION;
+    private static final String API_VERSION = "v1";
+    private static final String API_BASE = "api/" + API_VERSION;
 
-  @Override
-  public void configurePathMatch(PathMatchConfigurer configurer) {
-    WebMvcConfigurer.super.configurePathMatch(configurer);
-    // Adds the API prefix to all routes located inside a REST controller
-    configurer.addPathPrefix(API_BASE, HandlerTypePredicate.forAnnotation(RestController.class));
-  }
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        WebMvcConfigurer.super.configurePathMatch(configurer);
+        // Adds the API prefix to all routes located inside a REST controller
+        configurer.addPathPrefix(API_BASE, HandlerTypePredicate.forAnnotation(RestController.class));
+    }
 }
