f36b2db71199f612d9c137da4e1aa0b27a13b694
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-09 12:24:45 +0100

    Client-side handling of server-sent events

---
+++ front/src/events.ts
@@ -0,0 +1,85 @@
+import {
+  BASE_PATH,
+  type ApiError,
+  type Event,
+  type WidgetDeletionEvent,
+  type WidgetErrorEvent,
+  type WidgetRefreshEvent,
+  type WrappedApiError,
+} from "./api";
+
+interface UnknownEvent {
+  type: string;
+}
+
+export function init() {
+  const path = BASE_PATH.replace(/^(http|https)/, "ws");
+  const eventsWs = new WebSocket(`${path}/ws/events`);
+
+  eventsWs.onerror = (event) => console.error("Events WS errored: ", event);
+
+  eventsWs.onmessage = (event) => {
+    const data: Event | WrappedApiError = JSON.parse(event.data);
+
+    if ("type" in data && typeof data.type === "string") {
+      onEvent(data);
+    } else if ("error" in data) {
+      onError(data.error);
+    }
+  };
+}
+
+function onEvent(event: Event) {
+  console.debug(
+    `Recieved event of type ${event.type} at timestamp ${event.timestamp}`
+  );
+  switch (event.type) {
+    case "widgets/deletion":
+      onWidgetDeletion(event);
+      break;
+    case "widgets/error":
+      onWidgetError(event);
+      break;
+    case "widgets/refresh":
+      onWidgetRefresh(event);
+      break;
+    default:
+      console.error(`Unknown event type ${(event as UnknownEvent).type}`);
+  }
+}
+
+function onError(error: ApiError) {
+  if ("message" in error) {
+    console.error(`Received error ${error.code}: ${error.message}`);
+  } else {
+    console.error(`Receieved unknown error ${error.code}`);
+  }
+}
+
+function onWidgetError(event: WidgetErrorEvent) {
+  for (let error of event.data.widgets) {
+    if ("message" in error.error) {
+      console.error(
+        `Error ${error.error.code} for widget ${error.id}: ${error.error.message}`
+      );
+    } else {
+      console.error(`Error ${error.error.code} for widget ${error.id}`);
+    }
+  }
+}
+
+function onWidgetDeletion(event: WidgetDeletionEvent) {
+  for (let widgetId of event.data.widgetIds) {
+    console.debug(`Requested deletion of widget ${widgetId}`);
+
+    // inject your widget deleting code here
+  }
+}
+
+function onWidgetRefresh(event: WidgetRefreshEvent) {
+  for (let widget of event.data.widgets) {
+    console.debug(`Requested update of widget ${widget.id}`, widget.data);
+
+    // inject your widget update code here
+  }
+}
