04c40d9e56c00df3b31155c6edec3e4809d75863
Mathiso00 <mathis.onillon@epitech.eu>
2022-11-09 19:01:44 +0100

    Get list of service by userId

--- src/main/java/com/github/dashframe/dao/ServiceDAO.java
+++ src/main/java/com/github/dashframe/dao/ServiceDAO.java
@@ -1,11 +1,14 @@
 package com.github.dashframe.dao;
 
 import com.github.dashframe.models.Service;
+import com.github.dashframe.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public interface ServiceDAO extends JpaRepository<Service, Integer> {
     Service findById(int id);
-
+    ArrayList<Service> findByUser(User user);
 }
