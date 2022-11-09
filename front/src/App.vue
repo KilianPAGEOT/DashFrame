1e3f6ed4139826ef807fa69c8895971a12a9e50d
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-09 12:24:45 +0100

    Client-side handling of server-sent events

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,6 +2,8 @@
 import MainPageVue from "./components/MainPage.vue";
 import MainHeaderVue from "./components/MainHeader.vue";
 import { BASE_PATH, Configuration, InformationApi, ServicesApi } from "./api";
+import * as events from "./events";
+
 export default {
   components: {
     MainPageVue,
@@ -30,27 +32,9 @@ async function apiExample() {
   console.groupEnd();
 }
 
-function eventsExample() {
-  const path = BASE_PATH.replace(/^(http|https)/, "ws");
-  const eventsWs = new WebSocket("ws://127.0.0.1:8080/api/v1/ws/events");
-
-  eventsWs.onopen = () => {
-    console.log("Events WS opened, sending ping message");
-    eventsWs.send(
-      JSON.stringify({ from: "client", message: "lel, I'm the client" })
-    );
-  };
-  eventsWs.onclose = (event) =>
-    console.error("Events WS closed, reason: ", event.reason);
-  eventsWs.onerror = (event) => console.error("Events WS errored: ", event);
-  eventsWs.onmessage = (event) => {
-    console.log("server responded with", JSON.parse(event.data));
-  };
-}
-
 apiExample();
 try {
-  eventsExample();
+  events.init();
 } catch (error) {
   console.log(`epic failure: ${error}`);
 }
