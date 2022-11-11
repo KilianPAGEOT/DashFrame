8894826c2e41fe2f6c22a430b32ae833bd0ebc11
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-11 17:37:40 +0100

    Omit optional properties from JSON and format dates as RFC 3339

--- src/main/java/com/github/dashframe/config/ObjectMapperConfig.java
+++ src/main/java/com/github/dashframe/config/ObjectMapperConfig.java
@@ -1,6 +1,9 @@
 package com.github.dashframe.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.github.dashframe.RFC3339DateFormat;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +22,10 @@ public class ObjectMapperConfig {
     @Primary
     @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        var mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module());
+        mapper.setDateFormat(new RFC3339DateFormat());
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
+        return mapper;
     }
 }
