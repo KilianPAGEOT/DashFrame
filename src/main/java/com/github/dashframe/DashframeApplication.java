e6ba605ed5592372a4092c1693c1453ac145e57a
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-11 17:44:05 +0100

    Steam friends list widget data fetching implementation

--- src/main/java/com/github/dashframe/DashframeApplication.java
+++ src/main/java/com/github/dashframe/DashframeApplication.java
@@ -5,16 +5,6 @@ import com.github.dashframe.dao.ServiceDAO;
 import com.github.dashframe.dao.UserDAO;
 import com.github.dashframe.dao.WidgetDAO;
 import com.github.dashframe.dao.WidgetParameterDAO;
-import com.github.dashframe.models.Service;
-import com.github.dashframe.models.User;
-import com.github.dashframe.models.Widget;
-import com.github.dashframe.models.json.ServiceType;
-import com.github.dashframe.models.json.WidgetType;
-import com.github.dashframe.service.service.ServiceManagerProvider;
-import com.github.dashframe.service.service.SteamServiceManager;
-import com.github.dashframe.service.widget.WidgetManagerProvider;
-import com.github.dashframe.service.widget.steam.SteamFriendsListWidgetManager;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @Controller
@@ -58,31 +47,5 @@ public class DashframeApplication {
         body.put("client", new HashMap<>());
         body.put("server", new HashMap<>());
         return ResponseEntity.ok(body);
-    }
-
-    @Autowired
-    private ServiceManagerProvider serviceManagerProvider;
-
-    @Autowired
-    private WidgetManagerProvider widgetManagerProvider;
-
-    @ResponseBody
-    @GetMapping("/steam")
-    public Mono<?> steam() {
-        // FIXME: temporary route
-        SteamServiceManager serviceManager = (SteamServiceManager) this.serviceManagerProvider.forType(
-                ServiceType.STEAM
-            );
-        SteamFriendsListWidgetManager widgetManager = (SteamFriendsListWidgetManager) this.widgetManagerProvider.forType(
-                WidgetType.STEAM_FRIENDS_LIST
-            );
-        Service service = new Service(42, new User(), ServiceType.STEAM.getValue(), "76561198181932666", new Date());
-
-        assert serviceManager != null;
-        assert widgetManager != null;
-
-        return serviceManager
-            .open(service)
-            .map(serviceContext -> widgetManager.open(new Widget(), serviceContext).map(widgetManager::fetchData));
     }
 }
--- src/main/java/com/github/dashframe/service/widget/steam/SteamFriendsListWidgetManager.java
+++ src/main/java/com/github/dashframe/service/widget/steam/SteamFriendsListWidgetManager.java
@@ -1,21 +1,22 @@
 package com.github.dashframe.service.widget.steam;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.dashframe.models.Widget;
+import com.github.dashframe.models.json.SteamUser;
 import com.github.dashframe.models.json.WidgetType;
 import com.github.dashframe.service.service.SteamServiceManager;
 import com.github.dashframe.service.widget.WidgetContext;
 import com.github.dashframe.service.widget.WidgetManager;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class SteamFriendsListWidgetManager
-    implements
-        WidgetManager<SteamServiceManager.Context, SteamFriendsListWidgetManager.Context, List<SteamFriendsListWidgetManager.SteamFriend>> {
+    implements WidgetManager<SteamServiceManager.Context, SteamFriendsListWidgetManager.Context, List<SteamUser>> {
 
     @Override
     public WidgetType getType() {
@@ -28,7 +29,7 @@ public class SteamFriendsListWidgetManager
     }
 
     @Override
-    public Mono<List<SteamFriend>> fetchData(Context widgetContext) {
+    public Mono<List<SteamUser>> fetchData(Context widgetContext) {
         var client = widgetContext.serviceContext.client();
         var apiKey = widgetContext.serviceContext.apiKey();
         var steamid = widgetContext.serviceContext.steamid();
@@ -40,22 +41,58 @@ public class SteamFriendsListWidgetManager
                     .path("ISteamUser/GetFriendList/v0001")
                     .queryParam("key", apiKey)
                     .queryParam("steamid", steamid)
+                    .queryParam("relationship", "friend")
                     .build()
             )
             .retrieve()
-            .bodyToMono(RawData.class)
-            .map(rawData -> rawData.friendsList.friends);
+            .bodyToMono(FriendsListResponse.class)
+            .flatMapMany(response -> {
+                List<SteamId> rawFriends = response.friendsList.friends;
+                // Steam's API expect a list of comma-separated steam IDs as a parameter
+                String ids = rawFriends.stream().map(SteamId::steamId).collect(Collectors.joining(","));
+
+                return client
+                    .get()
+                    .uri(uriBuilder ->
+                        uriBuilder
+                            .path("ISteamUser/GetPlayerSummaries/v0002")
+                            .queryParam("key", apiKey)
+                            .queryParam("steamids", ids)
+                            .build()
+                    )
+                    .retrieve()
+                    .bodyToMono(PlayerSummariesResponse.class)
+                     // convert the response to a flux of Raw Steam users for easier post-processing
+                    .flatMapIterable(summaries -> summaries.response.players);
+            })
+            .map(raw ->
+                // Convert the users returned by Steam's API to the one our front-end expects
+                new SteamUser(
+                    raw.steamId(),
+                    raw.name(),
+                    raw.profileURL(),
+                    raw.avatar32x32(),
+                    raw.avatar64x64(),
+                    raw.avatar184x184(),
+                    raw.status(),
+                    // omit the `lastLogoff` field when the user is offline, or format it as a proper date otherwise
+                    raw.status() == SteamUser.Status.ONLINE
+                        ? Optional.empty()
+                        : Optional.of(new Date(raw.lastLogoff() * 1000))
+                )
+            )
+            .collect(Collectors.toList());
     }
 
     public record Context(SteamServiceManager.Context serviceContext, Widget widget) implements WidgetContext {}
 
-    private record RawData(@JsonProperty("friendslist") RawFriendsList friendsList) {}
+    private record FriendsListResponse(@JsonProperty("friendslist") FriendsList friendsList) {}
 
-    private record RawFriendsList(List<SteamFriend> friends) {}
+    private record FriendsList(List<SteamId> friends) {}
 
-    public record SteamFriend(
-        String steamid,
-        String relationship,
-        @JsonFormat(shape = JsonFormat.Shape.NUMBER, pattern = "s") Date friendSince
-    ) {}
+    private record SteamId(@JsonProperty("steamid") String steamId) {}
+
+    private record PlayerSummariesResponse(PlayerList response) {}
+
+    private record PlayerList(List<SteamUser.Raw> players) {}
 }
