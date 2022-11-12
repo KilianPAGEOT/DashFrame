6afa9d15ae36d7a6d0a2d453bd6064efe493d09c
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-12 17:08:19 +0100

    Send periodic widget refresh events using WebSockets

---
+++ src/main/java/com/github/dashframe/config/SchedulingConfig.java
@@ -0,0 +1,30 @@
+package com.github.dashframe.config;
+
+import com.github.dashframe.service.event.EventReactor;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig implements SchedulingConfigurer {
+
+    @Autowired
+    private EventReactor reactor;
+
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+        taskRegistrar.addTriggerTask(this.reactor::tick, this.reactor::nextTick);
+    }
+}
