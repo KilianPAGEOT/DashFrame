0e7bbed0c0f0456e9f3d3dd30ddb105aadf6a857
Yanis Guaye <yanis.guaye@epitech.eu>
2022-11-13 23:34:14 +0100

    Basic Steam friends list widget update logic

--- front/src/components/Steam/FriendsList.vue
+++ front/src/components/Steam/FriendsList.vue
@@ -1,3 +1,36 @@
 <template>
   <p>Steam Friends List</p>
+  <ul v-for="friend in friendsList" :key="friend.name">
+    <li>{{ friend.name }} loll</li>
+  </ul>
 </template>
+
+<script lang="ts">
+import type { SteamUser } from "@/api";
+import { widgetsData, widgetsUpdater } from "@/events";
+
+export default {
+  props: {
+    widgetId: Number,
+  },
+  computed: {
+    friendsList(): SteamUser[] {
+      if (this.widgetId == null) return [];
+      const data = widgetsData.get(this.widgetId) as
+        | SteamUser[]
+        | null
+        | undefined;
+      return data ?? [];
+    },
+  },
+  created() {
+    if (this.widgetId) {
+      widgetsUpdater.set(this.widgetId, () => {
+        this.$forceUpdate();
+      });
+    }
+  },
+};
+</script>
+
+<style></style>
